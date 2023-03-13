7e22aa416d7dca25b783c769dbb675b33fb2a955
LizekaD <lizekadada@gmail.com>
2023-03-13 14:26:57 -0700

    t0

---
+++ 0x03-python-data_structure/lists.h
@@ -0,0 +1,19 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+print_list_integer(my_list=[]);
+element_at(my_list, idx);
+replace_in_list(my_list, idx, element);
+print_reversed_list_integer(my_list=[]);
+new_in_list(my_list, idx, element);
+no_c(my_string);
+print_matrix_integer(matrix=[[]]);
+add_tuple(tuple_a=(), tuple_b=());
+multiple_returns(sentence);
+max_integer(my_list=[]);
+divisible_by_2(my_list=[]);
+delete_at(my_list=[], idx=0);
+is_palindrome(listint_t **head);
+print_python_list_info(PyObject *p);
+
+#endif
